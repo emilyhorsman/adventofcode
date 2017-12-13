c0403e18fa22200abcb865b894f8fd129614f83f
Emily Horsman <me@emilyhorsman.com>
2017-12-12 22:08:18 -0500

    Add part 2 of day 4 solution

--- 2017/day4.hs
+++ 2017/day4.hs
@@ -1,13 +1,28 @@
-isValid :: [String] -> Bool
-isValid [] = True
-isValid (x : xs) =
-    if elem x xs then False else isValid xs
+import           Data.List  (find, sort)
+import           Data.Maybe (isJust)
+
+type Cmp = String -> String -> Bool
 
 
-count :: String -> Int
-count input =
-    length $ filter isValid $ map words $ lines input
+isValid :: Cmp -> [String] -> Bool
+isValid _ [] = True
+isValid cmp (x : xs) =
+    if isJust (find (cmp x) xs) then False else isValid cmp xs
 
 
-solve :: FilePath -> IO Int
-solve path = count <$> readFile path
+count :: Cmp -> String -> Int
+count cmp input =
+    length $ filter (isValid cmp) $ map words $ lines input
+
+
+solve :: Cmp -> FilePath -> IO Int
+solve cmp path = count cmp <$> readFile path
+
+
+isAnagram :: String -> String -> Bool
+isAnagram a b =
+    sort a == sort b
+
+
+solve1 = solve (==)
+solve2 = solve isAnagram
