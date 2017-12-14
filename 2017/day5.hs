08b33d36f964c204c9847354c0d694ac58260d4e
Emily Horsman <me@emilyhorsman.com>
2017-12-13 22:55:44 -0500

    Complete day 5 solution

--- 2017/day5.hs
+++ 2017/day5.hs
@@ -6,16 +6,23 @@ parse :: String -> [Int]
 parse = map read . lines
 
 
-compute pointer steps instructions = do
+compute fDiff pointer steps instructions = do
     jump <- M.read instructions pointer
-    M.modify instructions (+ 1) pointer
+    M.modify instructions (+ (fDiff jump)) pointer
     let target = pointer + jump
     let nextSteps = steps + 1
-    if target >= M.length instructions || target < 0 then return nextSteps else compute target nextSteps instructions
+    if target >= M.length instructions || target < 0 then return nextSteps else compute fDiff target nextSteps instructions
+
+
+solve1 = compute (\_ -> 1) 0 0
+solve2 = compute (\jump -> if jump >= 3 then -1 else 1) 0 0
 
 
 main = do
     instructions <- parse <$> readFile "day5.1.input"
     vector <- thaw $ fromList instructions
-    s <- compute 0 0 vector
+    s <- solve1 vector
     print s
+    vector' <- thaw $ fromList instructions
+    s' <- solve2 vector'
+    print s'
